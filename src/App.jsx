ef04e5488cc019ad356dc508eda977c6c6a22e2b
Ayush Yadav <infoayushyadav96@gmail.com>
2023-05-21 12:19:26 +0530

    Added Particles effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,78 @@
-import { useContext } from "react";
+import { useCallback, useContext } from "react";
 import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
 import Intro from "./components/intro/Intro";
 import ProductList from "./components/productList/ProductList";
 import Toggle from "./components/toggle/Toggle";
 import { ThemeContext } from "./context";
+import { loadFull } from "tsparticles";
+import Particles from "react-tsparticles";
 
 const App = () => {
+  const options = {
+    particles: {
+      number: {
+        value: 30,
+        density: {
+          enable: true,
+          area: 3000,
+        },
+      },
+      color: {
+        value: ["#2EB67D", "#ECB22E", "#E01E5B", "#36C5F0", "#ec2082"],
+      },
+      shape: {
+        type: "circle",
+      },
+      opacity: {
+        value: 1,
+      },
+      size: {
+        value: { min: 1, max: 5 },
+      },
+      links: {
+        enable: true,
+        distance: 150,
+        color: "#808080",
+        opacity: 0.4,
+        width: 1,
+      },
+      move: {
+        enable: true,
+        speed: 2,
+        direction: "circular",
+        random: false,
+        straight: false,
+        outModes: "out",
+      },
+    },
+    interactivity: {
+      events: {
+        onHover: {
+          enable: true,
+          mode: "grab",
+        },
+        onClick: {
+          enable: false,
+          mode: "push",
+        },
+      },
+      modes: {
+        grab: {
+          distance: 160,
+          links: {
+            opacity: 1,
+          },
+        },
+        push: {
+          quantity: 4,
+        },
+      },
+    },
+  };
+  const particlesInit = useCallback(async (engine) => {
+    await loadFull(engine);
+  }, []);
   const theme = useContext(ThemeContext);
   const darkMode = theme.state.darkMode;
   return (
@@ -16,6 +82,7 @@ const App = () => {
         color: darkMode && "white",
       }}
     >
+      <Particles options={options} init={particlesInit} />
       <Toggle />
       <Intro />
       <About />
--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,36 +1,35 @@
-import React from 'react'
-import "./intro.css"
-import Me from "../../img/me.png"
+import React from "react";
+import "./intro.css";
+import Me from "../../img/me.png";
 const Intro = () => {
-    return (
-        <div className="i">
-            <div className="i-left">
-            <div className="i-left-wrapper">
-                <h2 className="i-intro">Hello, My name is</h2>
-                <h1 className="i-name">Ayush Yadav</h1>
-                <div className="i-title">
-                    <div className="i-title-wrapper">
-                        <div className="i-title-item">Web Developer</div>
-                        <div className="i-title-item">UI/UX Designer</div>
-                        <div className="i-title-item">Android Developer</div>
-                        <div className="i-title-item">ML Enthusiast</div>
-                        <div className="i-title-item">Creater</div>
-                    </div>
-                </div>
-                <p className="i-desc">
-                I design and develop services to upskill my self of all sizes,
+  return (
+    <div className="i">
+      <div className="i-left">
+        <div className="i-left-wrapper">
+          <h2 className="i-intro">Hello, My name is</h2>
+          <h1 className="i-name">Ayush Yadav</h1>
+          <div className="i-title">
+            <div className="i-title-wrapper">
+              <div className="i-title-item">Full Stack Developer</div>
+              <div className="i-title-item">UI/UX Designer</div>
+              <div className="i-title-item">Android Developer</div>
+              <div className="i-title-item">ML Enthusiast</div>
+              <div className="i-title-item">Creater</div>
+            </div>
+          </div>
+          <p className="i-desc">
+            I design and develop services to upskill my self of all sizes,
             specializing in creating stylish, modern websites, web services and
             online stores.
-                </p>
-            </div>
-            <svg
+          </p>
+        </div>
+        <svg
           width="75"
           height="75"
           viewBox="0 0 75 75"
           fill="none"
           stroke="black"
           className="i-scroll"
-        
           xmlns="http://www.w3.org/2000/svg"
         >
           <g id="scroll">
@@ -80,14 +79,13 @@ const Intro = () => {
             </g>
           </g>
         </svg>
-            </div>
-            <div className="i-right">
-                <div className="i-bg"></div>
-                <img src={Me} alt="" className="i-img" />
-            </div>
-            
-        </div>
-    )
-}
+      </div>
+      <div className="i-right">
+        <div className="i-bg"></div>
+        <img src={Me} alt="" className="i-img" />
+      </div>
+    </div>
+  );
+};
 
-export default Intro
+export default Intro;
